4d97693aa32a2b47701c2c1412a6521221f13a6c
Ignacio <as.ignasi97@gmail.com>
2021-04-20 13:29:03 +0200

    terminado lo de mascota

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,65 @@
-fun main(args: Array<String>) {
+fun main() {
+    var nombre : String?
+    var apellido : String?
+    var resp : Char?
+    var auxResp : String?
 
-    val nombreCompleto = NombreCompleto("Ignacio", "de Moreta")
-    val mascota = Mascota()
+    var nombreMascota : String?
+    var especie : String?
+    val numeroChip : Int?
+
+    print("Introducir nombre: ")
+    do{ nombre = readLine() }while (nombre.isNullOrEmpty())
+
+    print("Introducir apellido: ")
+    do{ apellido = readLine() }while (apellido.isNullOrEmpty())
+
+    val nombreCompleto = NombreCompleto(nombre, apellido)
+
+    print("¿Tiene mascota?(S/N)")
+    do{
+        do{
+            auxResp = readLine()
+        }while (auxResp.isNullOrEmpty())
+        resp = auxResp[0].toUpperCase()
+    }while (resp != 'S' && resp != 'N')
+
+
+    val mascota = if (resp == 'N') null else {
+        print("Introducir nombre de la mascota: ")
+        do{ nombreMascota = readLine() }while (nombreMascota.isNullOrEmpty())
+
+        print("Introducir especie: ")
+        do{ especie = readLine() }while (especie.isNullOrEmpty())
+
+        numeroChip = (0..10000).random()
+        print("Su número de chip es: $numeroChip\n")
+
+        Mascota(nombreMascota, especie, numeroChip)
+    }
+
     val persona = Persona(nombreCompleto, mascota)
 
+    if(persona.mascota != null)
+        println("${persona.nombreCompleto} si tiene una mascota con los siguientes datos: ${persona.mascota}")
+    else
+        println("${persona.nombreCompleto} no tiene una mascota")
 
 
-    persona.edad = 30
-    persona.setDni("12345678H")
-    println(persona)
+    persona.setDni("11231121H")
+
+
 
 }
 
-class Persona(val nombreCompleto : NombreCompleto, val mascota: Mascota){
-    var edad = 18
+class NombreCompleto(var nombre : String, var apellido : String){
+    override fun toString(): String {
+        return "$nombre $apellido"
+    }
+}
+
+class Persona(val nombreCompleto : NombreCompleto, val mascota: Mascota?){
+    private var edad = 18
     private var dni : String? = null
 
     fun setDni(dni: String) {
@@ -23,51 +69,17 @@ class Persona(val nombreCompleto : NombreCompleto, val mascota: Mascota){
             println("El dni no se modifico, debe tener 8 digitos y acabar por una letra")
     }
 
-
     override fun toString(): String {
-
-        return "Soy ${nombreCompleto.nombre}, con DNI $dni y "
+        return "Soy ${nombreCompleto.nombre} ${nombreCompleto.apellido}, con DNI $dni y $edad años."
     }
-
-
 }
-
-class NombreCompleto(var nombre : String, var apellido : String)
-
 
 //una persona tiene 1 o ninguna mascota. La mascota tiene nombre, pertenece a una especie y tiene un número de chip
 
-class Mascota(){
-    private var nombreMascota: String? = null
-    private var especie : String? = null
-    private var numeroChip : Int? = null
-
-    //nombreMascota
-    fun getNombreMascota() : String?{
-        nombreMascota?.let { return it }?: run{ return null }
+class Mascota(private var nombreMascota: String?, private var especie: String?, private var numeroChip: Int?){
+    override fun toString(): String {
+        return "Nombre de mascota: $nombreMascota \nEspecie: $especie\nNúmero de chip: $numeroChip"
     }
-    fun setNombreMascota(nombreMascota: String){
-        this.nombreMascota = nombreMascota
-    }
-
-    //especie
-    fun getEspecie() : String?{
-        especie?.let { return it }?: run{ return null }
-    }
-    fun setEspecie(especie : String){
-        this.especie = especie
-    }
-
-    //numeroChip
-    fun getNumeroChip() : Int?{
-        numeroChip?.let { return it }?: run{ return null }
-    }
-    fun setNumeroChip(numeroChip : Int){
-        this.numeroChip = numeroChip
-    }
-
-
-
 }
 
 
